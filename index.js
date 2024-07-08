88ccf4a6987643c3a8b2e8c8ce20a744ee4831be
Edwin <moyiedwin8@gmail.com>
2024-07-08 14:18:39 +0300

    adds elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+ main.remove('id')
+ let element = newHeader;
+const newHeader = document.createElement('h1')
+newHeader.textContent = 'New Rules'
+document.body.appendChild(newHeader)
+ newHeader.id='victory'
+ newHeader.textContent = 'is the champion'
